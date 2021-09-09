b094800541a393691368ada6b3c8428ae0e3695f
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-09 18:03:08 -0300

    Video 2.2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_pom');
