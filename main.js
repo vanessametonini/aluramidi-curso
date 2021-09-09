a07c66cbce9884d9050ea45934e6ecb7259bebdc
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-09 19:14:46 -0300

    Video 3.2

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
