3b3aa08be80789d4544d8b90c4a3e890c70f931b
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-14 12:17:46 -0300

    Video 4.3

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ let contador = 0;
 //enquanto
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSom;
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
 
     contador = contador + 1;
 
