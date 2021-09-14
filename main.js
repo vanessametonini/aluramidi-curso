8d59bda72f856d686481a6ba90bac8198e527251
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-14 11:33:45 -0300

    Video 4.1

--- main.js
+++ main.js
@@ -5,4 +5,15 @@ function tocaSomPom () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
