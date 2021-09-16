a60bfaafc0e03692ed41a3e90a3509d4823d9e22
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-16 10:21:26 -0300

    Video 4.5

--- main.js
+++ main.js
@@ -4,24 +4,15 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-//enquanto
-while (contador < listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador = contador + 1;
-
-    //console.log(contador);
 
 }
