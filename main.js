8aabf560b57f1c3d0e29a3bf3e2798f1296b6d7a
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-29 12:01:51 -0300

    Video 5.2

--- main.js
+++ main.js
@@ -15,8 +15,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
     tecla.onkeyup = function () {
