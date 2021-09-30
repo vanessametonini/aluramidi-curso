a5b4b288f37baf563f985725c03b526ee242fda6
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-09-30 16:49:33 -0300

    Video 5.5

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
